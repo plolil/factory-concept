b32d6f83258ed44c583dac45bf8c5a4f8fa1d94a
BuildTools <unconfigured@null.spigotmc.org>
2020-12-21 14:32:02 -0500

    finish bootstrap code

--- main.h
+++ main.h
@@ -7,7 +7,13 @@
 #include <SDL2/SDL_ttf.h>
 #include <SDL2/SDL_timer.h>
 #include <SDL2/SDL_thread.h>
+#include <SDL2/SDL_image.h>
 
 #include <stdio.h>
+
+typedef unsigned int bool;
+
+#define true 1
+#define false 0
 
 #endif
