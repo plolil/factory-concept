2f2a13b2703e2d91d9e8b1964ebc64b9aba6f9df
BuildTools <unconfigured@null.spigotmc.org>
2020-12-21 14:17:07 -0500

    fixing merge issues

--- main.h
+++ main.h
@@ -1,19 +1,13 @@
-<<<<<<< HEAD
 #ifndef MAIN_H
 #define MAIN_H
 
 #define NO_STDIO_REDIRECT
 
-=======
->>>>>>> aa34b9be7ea45098aae4cb97f506f499251e9441
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_ttf.h>
 #include <SDL2/SDL_timer.h>
 #include <SDL2/SDL_thread.h>
-<<<<<<< HEAD
 
 #include <stdio.h>
 
 #endif
-=======
->>>>>>> aa34b9be7ea45098aae4cb97f506f499251e9441
