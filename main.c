9327b4bb67de8ef960099dd15af8c53b1a15705f
BuildTools <unconfigured@null.spigotmc.org>
2020-12-21 14:28:20 -0500

    SDL2 bare bootstrap code with font

--- main.c
+++ main.c
@@ -1,5 +1,29 @@
-#include "main.h"
+int main(int argc, char ** argv)
+{
+	bool quit = false;
+	SDL_Event event;
 
-int main() {
-  return 0;
+	SDL_Init(SDL_INIT_VIDEO); TTF_Init();
+
+	TTF_Font * font = TTF_OpenFont("assets/font.ttf", 32);
+	SDL_Window * screen = SDL_CreateWindow("Factory Game",
+		SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, 0);
+	SDL_Renderer * renderer = SDL_CreateRenderer(screen, -1, 0);
+	while (!quit){
+		Uint32 start = SDL_GetTicks();
+		while (SDL_PollEvent(&event)) {
+			switch (event.type){
+				case SDL_QUIT:
+					quit = true;
+					break;
+			}
+		}
+		SDL_RenderClear(renderer);
+	}
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(screen);
+	TTF_CloseFont(font);
+	TTF_Quit();
+	SDL_Quit();
+	return 0;
 }
