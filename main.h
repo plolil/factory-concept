e8e2bccfe512d788a390278325418414a64d4fdc
BuildTools <unconfigured@null.spigotmc.org>
2020-12-21 14:13:37 -0500

    setting up the sdl bootstrap code

--- main.h
+++ main.h
@@ -1,1 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
 
+#define NO_STDIO_REDIRECT
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
+#include <SDL2/SDL_timer.h>
+#include <SDL2/SDL_thread.h>
+
+#include <stdio.h>
+
+#endif
