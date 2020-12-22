2169a53f02da6429cb256b709a6be3a34b8d4ebc
winrg <julianshipp4@gmail.com>
2020-12-21 21:30:22 -0500

    working on the engine

--- engine.hpp
+++ engine.hpp
@@ -1,10 +1,42 @@
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
+#include <SDL2/SDL_timer.h>
+#include <SDL2/SDL_thread.h>
+#include <SDL2/SDL_image.h>
+#include <vector>
+
+
 namespace engine {
-	class entity {
+	template <typename T>
+	class Vector2 {
 		public:
-		float x;
-		float y;
-		long type;
+		T x;
+		T y;
+		Vector2(T xin, T yin) {
+			x = xin;
+			y = yin;
+		}
+	};
+	class component {
+		public:
+		int setup();
+		int start();
+		int update(Uint32 DeltaT);
+		int stop();
+		int setdown();
+	};
+
+	class entity {
+		protected:
+		std::vector<component> components;
+		public:
+		Vector2<float> *pos;
 		long id;
+		bool visual;
+		entity(long id, float startingx, float startingy);
+	};
+
+	class tile {
 		
 	};
 }
--- main.hpp
+++ main.hpp
@@ -12,4 +12,6 @@
 #include <iostream>
 #include <string>
 
+#include "engine.hpp"
+
 #endif
