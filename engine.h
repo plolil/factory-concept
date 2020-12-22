57c90310d9810a2b0a010e153d80984f462132f7
winrg <julianshipp4@gmail.com>
2020-12-22 12:04:12 -0500

    working on backend junk

--- engine.h
+++ engine.h
@@ -7,12 +7,15 @@
 #include <SDL2/SDL_thread.h>
 #include <SDL2/SDL_image.h>
 
+#include "tasks.h"
+
 typedef struct engine {
 	SDL_Window * window;
 	SDL_Renderer * renderer;
 	SDL_Thread * Athread;
 	SDL_Thread * Bthread;
 	SDL_Thread * Cthread;
+	
 } ENGINE_engine;
 
 #endif
--- tasks.h
+++ tasks.h
@@ -1,8 +1,26 @@
 #ifndef TASKS_H
 #define TASKS_H
 
-typedef struct taskstore {
+typedef struct task {
+	int * funcptr;
+	void * affected;
+	int * extraA;
+	int * extraB;
+	char valid;
+	char inprogress;
+	int id;
+} TASKS_task;
 
+typedef struct taskstore {
+	TASKS_task tasklist[10];
+	int numtasks;
 } TASKS_taskstore;
+
+typedef unsigned int TASKS_taskid;
+
+TASKS_taskstore *TASKS_newstore();
+int TASKS_pushtask(TASKS_taskstore* store, int * func, void * target, int * paramA, int * paramB, TASKS_taskid *);
+int TASKS_poptask();
+
 
 #endif
