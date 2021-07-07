dff46575c5be3a033982a578031d69d3e1625e2e
winrg <julianshipp4@gmail.com>
2021-07-07 16:05:29 -0400

    enginecode/main.cpp

--- enginecode/main.cpp
+++ enginecode/main.cpp
@@ -1,3 +1,11 @@
 #include <libxml/parser.h>
+#include <fstream>
+
 #include <main.hpp>
-int main() {return 0;}
+
+int main(int argc, char ** argv) {
+  xmlParserCtxtPtr currentscene = xmlNewParserCtxt();
+  std::fstream CSfile("main.xml", std::fstream::in);
+  
+  return 0;
+}
